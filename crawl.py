0366687276de19fcb59c35ca84fdb69633242170
TMInstaller <ajvldmlqjqclr@naver.com>
2022-12-03 20:38:28 +0900

    crawl.py 작성 게시글 제목 긁어오기

--- crawl.py
+++ crawl.py
@@ -1,9 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen("https://news.naver.com")
+html = urlopen("https://time-map-installer.tistory.com/")
 
 bsObject = BeautifulSoup(html, "html.parser")
 
-for link in bsObject.find_all('a'):
-    print(link.text.strip(), link.get('href'))
+for link in bsObject.find_all('span', class_='title'):
+    print(link.text.strip(), link.get('span'))
